6f716cef324cec2598ce5ea546e904e6d627f90f
Mohamed Ziata <wakematta@gmail.com>
2017-02-17 11:36:52 +0100

    :tada: Initial commit (bundle gem)

---
+++ lib/fckeditor/rails.rb
@@ -0,0 +1,7 @@
+require "fckeditor/rails/version"
+
+module Fckeditor
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/fckeditor/rails/version.rb
@@ -0,0 +1,5 @@
+module Fckeditor
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
